cb478fd0bf9ad3d26bd774d9a8a4b515185f2da8
Lawrence Adrian De Belen <ltdebelen@gmail.com>
2020-01-14 22:18:59 -0600

    Cleaning up files

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,2 +1,16 @@
-console.log("HELLO");
-console.log(moment());
+let trEl = $("tr");
+
+trEl.click(function(e) {
+  //   console.log(e.target);
+  if (e.target.matches("button")) {
+    const rowID = $(this).data("id");
+    console.log(rowID);
+    // let todoObj = {
+    //     id:
+    //     activity:
+    // };
+    // saveTodo(todoObj);
+  }
+
+  id = act;
+});
