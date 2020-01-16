97243a927ed998acceebeea6208362f39ef0ed5d
Lawrence Adrian De Belen <ltdebelen@gmail.com>
2020-01-15 20:45:05 -0600

    Adding logic for hour comparison

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,27 @@
 const dateToday = moment().format("dddd, MMMM Do");
+const currentHour = moment().format("H");
+console.log(currentHour);
 let events = [];
 
 // Display current date in header
 $("#currentDay").text(dateToday);
+
+// change css styling and behavior based on current hour
+let eventHours = [19, 20, 21];
+eventHours.forEach(hour => {
+  // compare currentHour to the value of div id
+  const divHourVal = $("#div-" + hour).data("hour");
+  if (currentHour > divHourVal) {
+    // event is already done
+    $("#div-" + hour).css("background", "gray");
+  } else if (currentHour == divHourVal) {
+    // event is currently happening
+    $("#div-" + hour).css("background", "red");
+  } else if (currentHour < divHourVal) {
+    // event is going to happen
+    $("#div-" + hour).css("background", "green");
+  }
+});
 
 // Adding on click function when saving events
 $(".row").on("click", function(e) {
