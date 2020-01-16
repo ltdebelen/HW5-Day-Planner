20f86cf4ce7c9ec3adac0fa59193c5611e0ac14d
Lawrence Adrian De Belen <ltdebelen@gmail.com>
2020-01-15 20:19:31 -0600

    Adding functions for saving items to localStorage and populating them after page reload

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,39 @@
-let trEl = $("tr");
+const dateToday = moment().format("dddd, MMMM Do");
+let events = [];
 
-trEl.click(function(e) {
-  //   console.log(e.target);
+// Display current date in header
+$("#currentDay").text(dateToday);
+
+// Adding on click function when saving events
+$(".row").on("click", function(e) {
   if (e.target.matches("button")) {
     const rowID = $(this).data("id");
-    console.log(rowID);
-    // let todoObj = {
-    //     id:
-    //     activity:
-    // };
-    // saveTodo(todoObj);
+    const rowEvent = $("#event-" + rowID).val();
+    saveEvent(rowID, rowEvent);
   }
-
-  id = act;
 });
+
+// Save events in localStorage
+function saveEvent(id, event) {
+  let eventObj = {
+    id: id,
+    event: event
+  };
+
+  events.push(eventObj);
+  localStorage.setItem("events", JSON.stringify(events));
+  alert("Event has been saved");
+}
+
+// Populate hourly task from localStorage values
+function populateEventRows() {
+  const eventsArr = JSON.parse(localStorage.getItem("events"));
+
+  if (eventsArr != null) {
+    eventsArr.forEach(event => {
+      $("#event-" + event.id).val(event.event);
+    });
+  }
+}
+
+populateEventRows();
